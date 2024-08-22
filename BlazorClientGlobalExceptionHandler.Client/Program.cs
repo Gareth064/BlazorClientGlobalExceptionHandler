d4c1eab986baa3d36aa7f961e80d609a9967f24e
Gareth Doherty <gareth.doherty@outlook.com>
2024-08-22 22:30:30 +0100

    Updated with MudBlazor

--- BlazorClientGlobalExceptionHandler.Client/Program.cs
+++ BlazorClientGlobalExceptionHandler.Client/Program.cs
@@ -1,14 +1,17 @@
 using BlazorClientGlobalExceptionHandler.Client.Services.Logging;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddLogging(logging =>
 {
-    //if (builder.HostEnvironment.IsProduction())
-    logging.ClearProviders();
+    if (builder.HostEnvironment.IsProduction())
+        logging.ClearProviders();
 
     logging.AddProvider(new CustomLoggerProvider());
 });
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
